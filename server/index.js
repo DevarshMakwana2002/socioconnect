f67a44c9df2259f04811953213788e1f8b98de60
cuteishani <cuteishani2001@gmail.com>
2023-08-26 16:48:45 +0530

    added login functionality

--- server/index.js
+++ server/index.js
@@ -9,6 +9,7 @@ import morgan from "morgan";
 import path from "path";
 import { fileURLToPath } from "url";
 import { register } from "./controllers/auth.js";
+import authRoutes from "./routes/auth.js"
 
 // CONFIGURATION
 const __filename = fileURLToPath(import.meta.url);
@@ -37,6 +38,9 @@ const upload = multer({ storage });
 
 // ROUTES WITH FILES
 app.post("/auth/register", upload.single("picture"), register);
+
+// ROUTES 
+app.use("/auth",authRoutes)
 
 // MONGOOSE SETUP
 const PORT = process.env.PORT || 6001;
