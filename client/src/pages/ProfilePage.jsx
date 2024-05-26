cf4a727cc22dba80fabf925ab598004cd6497a03
DevarshMakwana2002 <devershppsv@gmail.com>
2024-05-26 15:45:05 +0530

    fresh start of FE

--- client/src/pages/ProfilePage.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { useParams } from "react-router-dom";
-
-const ProfilePage = () => {
-  const { id } = useParams();
-  return <div>ProfilePage {id} </div>;
-};
-
-export default ProfilePage;
