1f3fc37458f81cf105e27c12f58e11000a58beb6
DevarshMakwana2002 <devershppsv@gmail.com>
2024-05-28 23:27:36 +0530

    completed login page totally

--- client/src/index.js
+++ client/src/index.js
@@ -4,14 +4,12 @@ import "./index.css";
 import App from "./App";
 import { Provider } from "react-redux";
 import store from "./store";
-import { ToastContainer } from "react-toastify";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
       <App />
-      
     </Provider>
   </React.StrictMode>
 );
