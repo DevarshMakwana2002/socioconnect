9ae85a8c9036970224601c3f902b830b07e028dc
DevarshMakwana2002 <devershppsv@gmail.com>
2024-07-17 17:30:32 +0530

    fixed posts post api

--- server/index.js
+++ server/index.js
@@ -43,7 +43,7 @@ const upload = multer({ storage });
 
 // ROUTES WITH FILES
 app.post("/auth/register", upload.single("picture"), register);
-app.post("posts", verifyToken, upload.single("picture"), createPost);
+app.post("/posts", verifyToken, upload.single("picture"), createPost);
 
 // ROUTES
 app.use("/auth", authRoutes);
