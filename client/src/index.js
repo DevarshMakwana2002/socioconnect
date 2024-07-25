7d6f8b2658d97d3ecb568a8b0f2516c29294ec0f
DevarshMakwana2002 <devershppsv@gmail.com>
2024-07-25 22:14:56 +0530

    so simple

--- client/src/index.js
+++ client/src/index.js
@@ -6,6 +6,7 @@ import { Provider } from "react-redux";
 import store from "./store";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
+// so simple :(
 root.render(
   <React.StrictMode>
     <Provider store={store}>
